a88315beefee3f73404d65b156ea466d35bb44df
Tom Strother <tom_strother@yahoo.com>
2020-04-30 15:37:20 -0500

    Day3 working buttons

--- src/scripts/eventHandlers.js
+++ src/scripts/eventHandlers.js
@@ -2,31 +2,41 @@
 // pass .value into the fetch call
 // define location of returned results
 // loop results and run through the domComponent function
-// take each domComponent and append to dom location 
+// take each domComponent and append to dom location
 // Museum Search Event Handler
 
 
 
 // Parks Search Event Handler
-// document.querySelector("#parkSearchButton").addEventListener("click", function () {
-//   // console.log("NOOOOO")
-//   getArtWorks("parksFieldContainer").value
-//     .then(artworks => {
-//       for (let art of artworks) {
+document.querySelector("#parkSearchButton").addEventListener("click", event => {
+  let parksFieldValue = document.querySelector("#parksField").value;
+  getParks(parksFieldValue)
+    .then(parks => {
+      for (let i = 0; i < parks.length; i++) {
+        resultsContainer.innerHTML = parksHTMLFactory(parks[i], i)
+      }
+      console.log(resultsContainer)
+      document.getElementById("parks-save-button").addEventListener("click", event => {
+        itineraryResultsContainer.innerHTML = "Test"
+      })
+    })
+})
 
-//         resultsContainer.innerHTML = artComponent(art)
-//       }
-//     })
+// Print Park Search To Itinerary
+// document.getElementById("parks-save-button").addEventListener("click", function () {
+//   console.log("Save Button Works")
 // })
 
+// const parkSaveEvent = document.getElementByClass("parkTitle")
+// resultsContainer.innerHTML = parksHTMLFactory(park)
+
 // Art Search Event Handler
-document.querySelector("#artSearchButton").addEventListener("click", function () {
-  // console.log("Hello")
-  const fieldVariable = document.querySelector(".artsFieldContainer").value
-  getArtWorks(fieldVariable)
+document.querySelector("#artSearchButton").addEventListener("click", event => {
+  const artsFieldValue = document.getElementById("artsField").value
+  getArtWorks(artsFieldValue)
     .then(artworks => {
       for (let art of artworks) {
-        resultsContainer.innerHTML = artComponent(art)
+        resultsContainer.innerHTML += artComponent(art)
       }
     })
 })
--- src/scripts/parks.js
+++ src/scripts/parks.js
@@ -1,6 +1,6 @@
 // Parks API / Fetch
-function getParks() {
-  return fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+function getParks(park) {
+  return fetch(`https://data.nashville.gov/resource/74d7-b74t.json?park_name=${park}`)
     .then(parks => parks.json())
 }
 
